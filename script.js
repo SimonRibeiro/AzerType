ea836edad2eb3d00d56561f68b9ec2aa4bfebe81
Simon Ribeiro <ribeiro.simon@protonmail.com>
2024-05-27 16:39:59 +0200

    Proposition d'algorithme pour l'étape 3

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello World!")
+
+
+// Étape 1 : L’application propose un mot. 
+
+// Étape 2 : L’utilisateur tape ce mot au clavier. 
+
+/////// Étape 3 : Si le mot de l’utilisateur est exactement le même que le mot de l’application, alors on ajoute un point au score. //////
+
+
+
+// Si “motTapé identique à motProposé” == “vrai”
+
+// Alors “++score”
+
+// Sinon “rien”
+
+
+
+
+// Étape 4 : On passe au mot suivant.  
+
+// Étape 5 : On recommence à l’étape 1, jusqu’à ce que le temps soit écoulé. 
