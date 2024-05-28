5a5da4a73d3f1547a4007764410514c629cde845
Simon Ribeiro <ribeiro.simon@protonmail.com>
2024-05-28 10:16:27 +0200

    Amélioration du parcours des cases du tableau par la variable numero

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 console.log("Hello World!")
 
-
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 let score = 0
+let numero = 0
 
 // Étape 1 : L’application propose un mot. 
-let motApplication = listeMots[0]
+let motApplication = listeMots[numero]
 // Étape 2 : L’utilisateur tape ce mot au clavier. 
 let motUtilisateur = prompt("Entrez le mot : " + motApplication)
 
@@ -17,7 +17,7 @@ if (motUtilisateur === motApplication) {
     console.log("Vous avez fait une erreur de frappe.")
 }
 console.log(score)
-
+++numero
 ///////////////////////////
 ///// Need a for loop /////
 ///////////////////////////
@@ -26,7 +26,7 @@ console.log(score)
 // Étape 5 : On recommence à l’étape 1, jusqu’à ce que le temps soit écoulé. 
 
 
-motApplication = listeMots[1]
+motApplication = listeMots[numero]
 motUtilisateur = prompt("Entrez le mot : " + motApplication)
 
 if (motUtilisateur === motApplication) {
@@ -36,9 +36,9 @@ if (motUtilisateur === motApplication) {
     console.log("Vous avez fait une erreur de frappe.")
 }
 console.log(score)
+++numero
 
-
-motApplication = listeMots[2]
+motApplication = listeMots[numero]
 motUtilisateur = prompt("Entrez le mot : " + motApplication)
 
 if (motUtilisateur === motApplication) {
@@ -48,3 +48,4 @@ if (motUtilisateur === motApplication) {
     console.log("Vous avez fait une erreur de frappe.")
 }
 console.log(score)
+++numero
